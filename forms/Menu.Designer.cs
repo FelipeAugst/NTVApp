14492255318f4342a30e9c51ba35eff8a6b875fb
felipeaug92@gmail.com <felipeaug92@gmail.com>
2023-10-23 19:13:56 -0300

    Menu

--- forms/Menu.Designer.cs
+++ forms/Menu.Designer.cs
@@ -28,33 +28,140 @@
         /// </summary>
         private void InitializeComponent()
         {
-            pictureBox1 = new System.Windows.Forms.PictureBox();
+            pictureBox1 = new PictureBox();
+            menuStrip1 = new MenuStrip();
+            clientesToolStripMenuItem = new ToolStripMenuItem();
+            planosToolStripMenuItem = new ToolStripMenuItem();
+            novoToolStripMenuItem = new ToolStripMenuItem();
+            listarToolStripMenuItem = new ToolStripMenuItem();
+            editarToolStripMenuItem = new ToolStripMenuItem();
+            listarToolStripMenuItem1 = new ToolStripMenuItem();
+            buscarToolStripMenuItem = new ToolStripMenuItem();
+            deletarToolStripMenuItem = new ToolStripMenuItem();
+            incluirToolStripMenuItem = new ToolStripMenuItem();
+            excluirToolStripMenuItem = new ToolStripMenuItem();
+            configuraçõesToolStripMenuItem = new ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
-            this.SuspendLayout();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
             // 
             // pictureBox1
             // 
-            pictureBox1.Location = new System.Drawing.Point(305, 52);
+            pictureBox1.Location = new Point(305, 52);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new System.Drawing.Size(100, 50);
-            pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            pictureBox1.Size = new Size(100, 50);
+            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { clientesToolStripMenuItem, planosToolStripMenuItem, configuraçõesToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // clientesToolStripMenuItem
+            // 
+            clientesToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { novoToolStripMenuItem, editarToolStripMenuItem, listarToolStripMenuItem1, buscarToolStripMenuItem, deletarToolStripMenuItem });
+            clientesToolStripMenuItem.Name = "clientesToolStripMenuItem";
+            clientesToolStripMenuItem.Size = new Size(61, 20);
+            clientesToolStripMenuItem.Text = "Clientes";
+            // 
+            // planosToolStripMenuItem
+            // 
+            planosToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { incluirToolStripMenuItem, excluirToolStripMenuItem });
+            planosToolStripMenuItem.Name = "planosToolStripMenuItem";
+            planosToolStripMenuItem.Size = new Size(54, 20);
+            planosToolStripMenuItem.Text = "Planos";
+            // 
+            // novoToolStripMenuItem
+            // 
+            novoToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { listarToolStripMenuItem });
+            novoToolStripMenuItem.Name = "novoToolStripMenuItem";
+            novoToolStripMenuItem.Size = new Size(180, 22);
+            novoToolStripMenuItem.Text = "Novo";
+            // 
+            // listarToolStripMenuItem
+            // 
+            listarToolStripMenuItem.Name = "listarToolStripMenuItem";
+            listarToolStripMenuItem.Size = new Size(180, 22);
+            listarToolStripMenuItem.Text = "Listar";
+            // 
+            // editarToolStripMenuItem
+            // 
+            editarToolStripMenuItem.Name = "editarToolStripMenuItem";
+            editarToolStripMenuItem.Size = new Size(180, 22);
+            editarToolStripMenuItem.Text = "Editar";
+            // 
+            // listarToolStripMenuItem1
+            // 
+            listarToolStripMenuItem1.Name = "listarToolStripMenuItem1";
+            listarToolStripMenuItem1.Size = new Size(180, 22);
+            listarToolStripMenuItem1.Text = "Listar";
+            // 
+            // buscarToolStripMenuItem
+            // 
+            buscarToolStripMenuItem.Name = "buscarToolStripMenuItem";
+            buscarToolStripMenuItem.Size = new Size(180, 22);
+            buscarToolStripMenuItem.Text = "Buscar";
+            // 
+            // deletarToolStripMenuItem
+            // 
+            deletarToolStripMenuItem.Name = "deletarToolStripMenuItem";
+            deletarToolStripMenuItem.Size = new Size(180, 22);
+            deletarToolStripMenuItem.Text = "Deletar";
+            // 
+            // incluirToolStripMenuItem
+            // 
+            incluirToolStripMenuItem.Name = "incluirToolStripMenuItem";
+            incluirToolStripMenuItem.Size = new Size(180, 22);
+            incluirToolStripMenuItem.Text = "Incluir";
+            // 
+            // excluirToolStripMenuItem
+            // 
+            excluirToolStripMenuItem.Name = "excluirToolStripMenuItem";
+            excluirToolStripMenuItem.Size = new Size(180, 22);
+            excluirToolStripMenuItem.Text = "Excluir";
+            // 
+            // configuraçõesToolStripMenuItem
+            // 
+            configuraçõesToolStripMenuItem.Name = "configuraçõesToolStripMenuItem";
+            configuraçõesToolStripMenuItem.Size = new Size(96, 20);
+            configuraçõesToolStripMenuItem.Text = "Configurações";
+            // 
             // Menu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(pictureBox1);
-            this.Name = "Menu";
-            this.Text = "Menu";
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(menuStrip1);
+            Name = "Menu";
+            Text = "Menu";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
-            this.ResumeLayout(false);
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private PictureBox pictureBox1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem clientesToolStripMenuItem;
+        private ToolStripMenuItem novoToolStripMenuItem;
+        private ToolStripMenuItem listarToolStripMenuItem;
+        private ToolStripMenuItem editarToolStripMenuItem;
+        private ToolStripMenuItem planosToolStripMenuItem;
+        private ToolStripMenuItem listarToolStripMenuItem1;
+        private ToolStripMenuItem buscarToolStripMenuItem;
+        private ToolStripMenuItem deletarToolStripMenuItem;
+        private ToolStripMenuItem incluirToolStripMenuItem;
+        private ToolStripMenuItem excluirToolStripMenuItem;
+        private ToolStripMenuItem configuraçõesToolStripMenuItem;
     }
 }
